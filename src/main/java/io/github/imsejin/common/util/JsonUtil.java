d66224d32ba02ca658d11d6eebd45048d2dde628
ImSejin <hackeres123@gmail.com>
2020-05-21 23:06:23 +0900

    ♻️ Refactor: JSON utilities

--- src/main/java/io/github/imsejin/common/util/JsonUtil.java
+++ src/main/java/io/github/imsejin/common/util/JsonUtil.java
@@ -2,11 +2,9 @@ package io.github.imsejin.common.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.Spliterators;
@@ -17,29 +15,39 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
+/**
+ * JSON 유틸리티<br>
+ * JSON utilities
+ * 
+ * <p>
+ * JSON을 처리하는 유틸리티.<br>
+ * Utilities that process the JSON.
+ * </p>
+ * 
+ * @author SEJIN
+ */
 @UtilityClass
 public class JsonUtil {
 
-    @SneakyThrows(IOException.class)
-    public String readAllJson(Reader reader) {
-        StringBuilder sb = new StringBuilder();
-
-        int cp;
-        while ((cp = reader.read()) > 0) {
-            sb.append((char) cp);
-        }
-
-        return sb.toString();
-    }
-
-    @SneakyThrows(IOException.class)
+    /**
+     * URL에서 반환하는 JSON 형식의 문자열을 읽어 JsonObject로 변환한다.<br>
+     * Reads the JSON format string returned by the URL and converts it to JsonObject.
+     * 
+     * <pre>
+     * String uriText = "http://cdn.lezhin.com/episodes/snail/1.json?access_token=5be30a25-a044-410c-88b0-19a1da968a64";
+     * URL url = URI.create(uriText).toURL();
+     * 
+     * JsonObject json = JsonUtil.readJsonFromUrl(url);
+     * </pre>
+     */
+    @SneakyThrows({ IOException.class, JsonSyntaxException.class })
     public JsonObject readJsonFromUrl(URL url) {
-        try (InputStream in = url.openStream()) {
-            Reader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8")));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
             String jsonText = readAllJson(reader);
             JsonObject json = JsonParser.parseString(jsonText).getAsJsonObject();
 
@@ -47,11 +55,33 @@ public class JsonUtil {
         }
     }
 
+    /**
+     * Reader가 읽은 모든 문자들을 문자열로 변환한다.<br>
+     * Converts all characters read by Reader to string.
+     */
+    private String readAllJson(BufferedReader reader) {
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    /**
+     * JSON 형식의 문자열을 지정한 오브젝트로 변환한다.<br>
+     * Converts the JSON format string to the specified object.
+     * 
+     * <pre>
+     * String jsonText = "{\"id\":1011,\"list\":[{\"id\":10,\"name\":\"foo\"},{\"id\":11,\"name\":\"bar\"}]}";
+     * 
+     * T t = JsonUtil.toObject(jsonText, T.class);
+     * </pre>
+     */
+    @SneakyThrows(JsonSyntaxException.class)
     public <T> T toObject(String jsonText, Class<T> clazz) {
         return new Gson().fromJson(jsonText, clazz);
     }
 
     /**
+     * JsonArray를 지정한 오브젝트의 리스트로 변환한다.<br>
+     * Converts JsonArray to a list of the specified objects.
+     * 
      * <pre>
      * String jsonText = "{\"id\":1011,\"list\":[{\"id\":10,\"name\":\"foo\"},{\"id\":11,\"name\":\"bar\"}]}";
      * JsonObject jsonObject = JsonParser.parseString(jsonText).getAsJsonObject();
@@ -60,6 +90,7 @@ public class JsonUtil {
      * List<T> list = JsonUtil.toList(jsonArray, T.class);
      * </pre>
      */
+    @SneakyThrows(JsonSyntaxException.class)
     public <T> List<T> toList(JsonArray jsonArray, Class<T> clazz) {
         Gson gson = new Gson();
 
