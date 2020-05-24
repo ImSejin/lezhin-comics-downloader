6c26d4226379432265d4f8887935c5e3f0ceb509
ImSejin <hackeres123@gmail.com>
2020-05-24 14:07:45 +0900

    🍱 Add: constants

--- src/main/java/io/github/imsejin/common/Constants.java
+++ src/main/java/io/github/imsejin/common/Constants.java
@@ -12,6 +12,18 @@ public class Constants {
     public final String EPISODE_ID_URI_PREFIX = "https://www.lezhin.com/ko/comic/";
 
     /**
+     * 로그인 URI<br>
+     * the URI for login
+     */
+    public final String LOGIN_URI_PREFIX = "https://www.lezhin.com/ko/login/submit";
+
+    /**
+     * 성인용 웹툰의 각 회차 아이디를 얻을 수 있는 URI의 매개변수; 로그인 후 해당 웹툰 페이지로 리디렉션한다.<br>
+     * The parameter of URI to obtain ID for each adult episode; Redirects to the webtoon page after login.
+     */
+    public final String ADULT_EPISODE_ID_URI_PARAM = "?redirect=%2Fko%2Fadult%3Fredirect%3D%252Fko%252Fcomic%252F";
+
+    /**
      * 각 회차의 정보를 얻을 수 있는 URI의 접두사<br>
      * The prefix of URI to obtain information for each episode
      */
