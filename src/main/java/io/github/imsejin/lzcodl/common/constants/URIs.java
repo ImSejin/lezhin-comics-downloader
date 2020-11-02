747884b98dc823eb4471e2b0e86db396e9405415
ImSejin <hackeres123@gmail.com>
2020-11-02 11:02:19 +0900

    ♻️ Change: abstract method to method with body in `URIs`

--- src/main/java/io/github/imsejin/lzcodl/common/constants/URIs.java
+++ src/main/java/io/github/imsejin/lzcodl/common/constants/URIs.java
@@ -4,6 +4,8 @@ import io.github.imsejin.common.constant.interfaces.KeyValue;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 public enum URIs implements KeyValue {
@@ -16,53 +18,40 @@ public enum URIs implements KeyValue {
      *     https://www.lezhin.com/ko/login
      * }</pre>
      */
-    LOGIN("https://www.lezhin.com/{language}/login") {
-        @Override
-        public String get(String... param) {
-            return this.value().replaceFirst("\\{language}", param[0]);
-        }
-    },
+    LOGIN("https://www.lezhin.com/{language}/login"),
 
     /**
      * <pre>{@code
      *     https://www.lezhin.com/ko/locale/ko-KR?locale=ko-KR
      * }</pre>
      */
-    LOCALE("https://www.lezhin.com/{language}/locale/{locale}?locale={locale}") {
-        @Override
-        public String get(String... param) {
-            return this.value()
-                    .replaceFirst("\\{language}", param[0])
-                    .replaceAll("\\{locale}", param[1]);
-        }
-    },
+    LOCALE("https://www.lezhin.com/{language}/locale/{locale}?locale={locale}"),
 
     /**
      * 각 회차의 아이디를 얻을 수 있는 URI의 접두사<br>
      * The prefix of URI obtain ID for each episode
      *
-     * <pre>
-     * /comic/
-     * </pre>
+     * <pre>{@code
+     *     https://www.lezhin.com/ko/comic/redhood
+     * }</pre>
      */
-    COMIC("https://www.lezhin.com/{language}/comic/{comicName}") {
-        @Override
-        public String get(String... param) {
-            return this.value()
-                    .replaceFirst("\\{language}", param[0])
-                    .replaceFirst("\\{comicName}", param[1]);
-        }
-    },
+    COMIC("https://www.lezhin.com/{language}/comic/{comicName}"),
 
-    EPISODE("https://www.lezhin.com/{language}/comic/{comicName}/{episodeName}") {
-        @Override
-        public String get(String... param) {
-            return this.value()
-                    .replaceFirst("\\{language}", param[0])
-                    .replaceFirst("\\{comicName}", param[1])
-                    .replaceFirst("\\{episodeName}", param[2]);
-        }
-    };
+    /**
+     * <pre>{@code
+     *     https://www.lezhin.com/ko/comic/redhood/9
+     *     https://www.lezhin.com/ko/comic/redhood/e1
+     * }</pre>
+     */
+    EPISODE("https://www.lezhin.com/{language}/comic/{comicName}/{episodeName}"),
+
+    EXPIRED("https://www.lezhin.com/{language}/error/expired"),
+
+    LIB_COMIC("https://www.lezhin.com/{language}/library/comic/{locale}/{comicName}"),
+
+    LIB_EPISODE("https://www.lezhin.com/{language}/library/comic/{locale}/{comicName}/{episodeName}");
+
+    private static final Pattern pattern = Pattern.compile("\\{(.+?)}", Pattern.MULTILINE);
 
     private final String value;
 
@@ -81,6 +70,21 @@ public enum URIs implements KeyValue {
         return this.value;
     }
 
-    public abstract String get(String... param);
+    public String get(String... params) {
+        if (params == null || params.length == 0) return this.value;
+
+        Matcher matcher = pattern.matcher(this.value);
+
+        // Converts all variables to parameters.
+        String uri = this.value;
+        for (int i = 0; i < params.length && matcher.find(); i++) {
+            uri = uri.replaceAll("\\{" + matcher.group(1) + '}', params[i]);
+        }
+
+        // Validates all variables in URI are converted to parameters.
+        if (pattern.matcher(uri).find()) throw new RuntimeException("Template URI has not matched variable(s)");
+
+        return uri;
+    }
 
 }
