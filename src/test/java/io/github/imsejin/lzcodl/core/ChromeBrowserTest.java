a80e43546a6088fa46d0f7351c62e929feb3d173
ImSejin <hackeres123@gmail.com>
2020-11-09 09:40:10 +0900

    🧪 Update: test code

--- src/test/java/io/github/imsejin/lzcodl/core/ChromeBrowserTest.java
+++ src/test/java/io/github/imsejin/lzcodl/core/ChromeBrowserTest.java
@@ -1,21 +1,27 @@
 package io.github.imsejin.lzcodl.core;
 
 import io.github.imsejin.common.util.JsonUtils;
+import io.github.imsejin.lzcodl.common.constants.URIs;
 import io.github.imsejin.lzcodl.model.Product;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ChromeBrowserTest {
 
     @Test
+    @Order(1)
     public void getJson() {
         // given
         ChromeDriver driver = ChromeBrowser.getDriver();
 
         // when
-        driver.get("https://www.lezhin.com/ko/comic/snail");
+        driver.get(URIs.COMIC.get("ko", "snail"));
         driver.executeScript("localStorage.setItem('product', JSON.stringify(window.__LZ_PRODUCT__.product));");
         String product = driver.getLocalStorage().getItem("product");
 
