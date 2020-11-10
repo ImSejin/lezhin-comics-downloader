43d4ffa7ec02f47281498a5b67ba350375b9b62e
ImSejin <hackeres123@gmail.com>
2020-11-10 11:32:21 +0900

    ✨ Add: debugger for chrome browser

--- src/main/java/io/github/imsejin/lzcodl/core/ChromeBrowser.java
+++ src/main/java/io/github/imsejin/lzcodl/core/ChromeBrowser.java
@@ -1,15 +1,23 @@
 package io.github.imsejin.lzcodl.core;
 
 import io.github.imsejin.common.util.PathnameUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public final class ChromeBrowser {
 
     private static final String CHROME_DRIVER_PATHNAME;
+
+    private static ChromeOptions options = new ChromeOptions().addArguments(ChromeOption.getArguments());
 
     static {
         // Assigns chrome driver pathname.
@@ -25,20 +33,59 @@ public final class ChromeBrowser {
     private ChromeBrowser() {
     }
 
+    public static void debugging() {
+        List<String> arguments = ChromeOption.getArguments().stream()
+                .filter(it -> !it.equals(ChromeOption.HEADLESS.argument)).collect(toList());
+        options = new ChromeOptions().addArguments(arguments);
+    }
+
     public static ChromeDriver getDriver() {
         return SingletonLazyHolder.DRIVER;
     }
 
     private static class SingletonLazyHolder {
-        private static final ChromeDriver DRIVER = new ChromeDriver(
-                new ChromeOptions().addArguments(
-                        "--start-maximized",           // 전체화면으로 실행한다.
-                        "--disable-popup-blocking",    // 팝업화면을 무시한다.
-                        "--disable-default-apps",      // 기본 앱을 사용하지 않는다.
-                        "--ignore-certificate-errors", // 인증오류를 무시한다.
-                        "--headless",                  // CLI 환경에서도 실행할 수 있게 한다.
-                        "--incognito")                 // 프라이빗 모드로 실행한다.
-        );
+        private static final ChromeDriver DRIVER = new ChromeDriver(options);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ChromeOption {
+
+        /**
+         * 전체화면으로 실행한다.
+         */
+        START_MAXIMIZED("--start_maximized"),
+
+        /**
+         * 팝업화면을 무시한다.
+         */
+        DISABLE_POPUP_BLOCKING("--disable_popup_blocking"),
+
+        /**
+         * 기본 앱을 사용하지 않는다.
+         */
+        DISABLE_DEFAULT_APPS("--disable_default_apps"),
+
+        /**
+         * 인증 오류를 무시한다.
+         */
+        IGNORE_CERTIFICATE_ERRORS("--ignore_certificate_errors"),
+
+        /**
+         * CLI 환경에서도 실행할 수 있게 한다.
+         */
+        HEADLESS("--headless"),
+
+        /**
+         * 프라이빗 모드로 실행한다.
+         */
+        INCOGNITO("--incognito");
+
+        private final String argument;
+
+        public static List<String> getArguments() {
+            return Arrays.stream(values()).map(it -> it.argument).collect(toList());
+        }
     }
 
 }
