c404b89fff38cc80b56a7f1341d5aeb7246f7e4f
ImSejin <hackeres123@gmail.com>
2022-11-27 19:58:56 +0900

    Add: package `process`

---
+++ src/main/java/io/github/imsejin/dl/lezhin/process/ProcessContext.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2022 Sejin Im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.imsejin.dl.lezhin.process;
+
+import io.github.imsejin.common.util.ArrayUtils;
+import io.github.imsejin.common.util.ReflectionUtils;
+import io.github.imsejin.dl.lezhin.argument.impl.ContentName;
+import io.github.imsejin.dl.lezhin.argument.impl.DebugMode;
+import io.github.imsejin.dl.lezhin.argument.impl.EpisodeRange;
+import io.github.imsejin.dl.lezhin.argument.impl.Language;
+import io.github.imsejin.dl.lezhin.argument.impl.SaveAsJpeg;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProcessContext {
+
+    private static final List<Field> FIELDS = Arrays.stream(ProcessContext.class.getDeclaredFields())
+            .filter(it -> !Modifier.isStatic(it.getModifiers()))
+            .collect(toUnmodifiableList());
+
+    private Language language;
+
+    private ContentName contentName;
+
+    private EpisodeRange episodeRange;
+
+    private SaveAsJpeg saveAsJpeg;
+
+    private DebugMode debugMode;
+
+    /**
+     * Creates new instance.
+     *
+     * <p> There are some careful points. First, if the attributes have {@code null} element, it is ignored.
+     * Second, when an attribute matches one of the fields, the attribute is assigned to new instance and
+     * other attributes that is the same as that attribute is discarded.
+     *
+     * @param attributes attributes
+     * @return new context
+     */
+    public static ProcessContext create(Object... attributes) {
+        ProcessContext context = new ProcessContext();
+
+        if (ArrayUtils.isNullOrEmpty(attributes)) {
+            return context;
+        }
+
+        outer:
+        for (Field field : FIELDS) {
+            for (Object attribute : attributes) {
+                if (attribute == null) {
+                    continue;
+                }
+
+                if (field.getType().isAssignableFrom(attribute.getClass())) {
+                    ReflectionUtils.setFieldValue(context, field, attribute);
+
+                    // Discards other attributes whose type is the same as the matched attribute.
+                    continue outer;
+                }
+            }
+        }
+
+        return context;
+    }
+
+    public static ProcessContext of(ProcessContext context, Object... attributes) {
+        if (ArrayUtils.isNullOrEmpty(attributes)) {
+            return context;
+        }
+
+        if (Arrays.stream(attributes).allMatch(Objects::isNull)) {
+            return context;
+        }
+
+        Object[] originAttributes = new Object[]{
+                context.language,
+                context.contentName,
+                context.episodeRange,
+                context.saveAsJpeg,
+                context.debugMode,
+        };
+
+        // New attributes take precedence over the attributes of context.
+        Object[] prepended = ArrayUtils.prepend(originAttributes, attributes);
+        return create(prepended);
+    }
+
+}
---
+++ src/test/groovy/io/github/imsejin/dl/lezhin/process/ProcessContextSpec.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2022 Sejin Im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.imsejin.dl.lezhin.process
+
+import io.github.imsejin.dl.lezhin.argument.impl.ContentName
+import io.github.imsejin.dl.lezhin.argument.impl.DebugMode
+import io.github.imsejin.dl.lezhin.argument.impl.EpisodeRange
+import io.github.imsejin.dl.lezhin.argument.impl.Language
+import io.github.imsejin.dl.lezhin.argument.impl.SaveAsJpeg
+import spock.lang.Specification
+
+class ProcessContextSpec extends Specification {
+
+    def "Creates new context with attributes"() {
+        given:
+        def attributes = [language, contentName, episodeRange, saveAsJpeg, debugMode]
+
+        when:
+        def context = ProcessContext.create(attributes as Object[])
+
+        then:
+        context != null
+        context.language === language
+        context.contentName === contentName
+        context.episodeRange === episodeRange
+        context.saveAsJpeg === saveAsJpeg
+        context.debugMode === debugMode
+
+        where:
+        language                  | contentName                       | episodeRange                    | saveAsJpeg                     | debugMode
+        new Language(value: "ko") | new ContentName(value: "alpha")   | new EpisodeRange(value: "")     | new SaveAsJpeg(value: "false") | new DebugMode(value: "false")
+        new Language(value: "en") | new ContentName(value: "epsilon") | new EpisodeRange(value: "~25")  | new SaveAsJpeg(value: "true")  | new DebugMode(value: "false")
+        new Language(value: "ja") | new ContentName(value: "zeta")    | new EpisodeRange(value: "1~10") | new SaveAsJpeg(value: "true")  | new DebugMode(value: "true")
+    }
+
+    def "Returns given context with redundant attributes"() {
+        given:
+        def context = ProcessContext.create()
+
+        when:
+        def newContext = ProcessContext.of(context)
+
+        then:
+        newContext != null
+        newContext === context
+
+        when:
+        newContext = ProcessContext.of(context, null, null)
+
+        then:
+        newContext != null
+        newContext === context
+    }
+
+    def "Creates new context with other context and attributes"() {
+        given:
+        def attributes = [language, contentName, episodeRange, saveAsJpeg, debugMode]
+        def context = ProcessContext.create(attributes as Object[])
+
+        when:
+        def newContext = ProcessContext.of(context)
+
+        then:
+        newContext != null
+        newContext !== context
+        context.language === language
+        context.contentName === contentName
+        context.episodeRange === episodeRange
+        context.saveAsJpeg === saveAsJpeg
+        context.debugMode === debugMode
+
+        where:
+        language                  | contentName                       | episodeRange                    | saveAsJpeg                     | debugMode
+        new Language(value: "ko") | new ContentName(value: "alpha")   | new EpisodeRange(value: "")     | new SaveAsJpeg(value: "false") | new DebugMode(value: "false")
+        new Language(value: "en") | new ContentName(value: "epsilon") | new EpisodeRange(value: "~25")  | new SaveAsJpeg(value: "true")  | new DebugMode(value: "false")
+        new Language(value: "ja") | new ContentName(value: "zeta")    | new EpisodeRange(value: "1~10") | new SaveAsJpeg(value: "true")  | new DebugMode(value: "true")
+    }
+
+}
