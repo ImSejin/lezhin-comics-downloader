315d4538c05ea15085dae05676368de0d27983fe
imsejin <hackeres123@gmail.com>
2022-11-28 13:46:51 +0900

    Add: package `attribute`

---
+++ src/main/java/io/github/imsejin/dl/lezhin/attribute/Attribute.java
@@ -0,0 +1,10 @@
+package io.github.imsejin.dl.lezhin.attribute;
+
+import io.github.imsejin.dl.lezhin.process.ProcessContext;
+
+/**
+ * Marker interface which indicates that
+ * it will be recognized as a attribute of {@link ProcessContext}.
+ */
+public interface Attribute {
+}
