adc756408051806bb7486da68e11d960d733a77f
ImSejin <hackeres123@gmail.com>
2022-12-04 23:24:07 +0900

    Add: processor `ContentDirectoryProcessor`

---
+++ src/main/java/io/github/imsejin/dl/lezhin/process/impl/ContentDirectoryProcessor.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Sejin Im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.imsejin.dl.lezhin.process.impl;
+
+import io.github.imsejin.dl.lezhin.annotation.ProcessSpecification;
+import io.github.imsejin.dl.lezhin.attribute.impl.Content.Artist;
+import io.github.imsejin.dl.lezhin.common.Loggers;
+import io.github.imsejin.dl.lezhin.exception.AccessTokenNotFoundException;
+import io.github.imsejin.dl.lezhin.process.ProcessContext;
+import io.github.imsejin.dl.lezhin.process.Processor;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.util.stream.Collectors.joining;
+
+/**
+ * Processor for creating a directory of the content
+ *
+ * @since 3.0.0
+ */
+@RequiredArgsConstructor
+@ProcessSpecification(dependsOn = ContentInformationProcessor.class)
+public class ContentDirectoryProcessor implements Processor {
+
+    private final Path basePath;
+
+    @Override
+    public Void process(ProcessContext context) throws AccessTokenNotFoundException {
+        String contentTitle = context.getContent().getDisplay().getTitle();
+        String artists = context.getContent().getArtists().stream().map(Artist::getName)
+                .collect(joining(", "));
+        String directoryName = String.format("L_%s - %s", contentTitle, artists);
+
+        Path targetPath = this.basePath.resolve(directoryName);
+
+        if (!Files.isDirectory(targetPath)) {
+            Loggers.getLogger().debug("Create directory: {}", targetPath);
+
+            try {
+                Files.createDirectory(targetPath);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create directory: " + targetPath, e);
+            }
+        }
+
+        return null;
+    }
+
+}
