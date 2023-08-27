411749c466d5026b85527636fd2dc6c43b62891b
ImSejin <hackeres123@gmail.com>
2023-08-27 14:32:09 +0900

    test: update test case

--- src/test/groovy/io/github/imsejin/dl/lezhin/process/framework/ProcessorCreatorSpec.groovy
+++ src/test/groovy/io/github/imsejin/dl/lezhin/process/framework/ProcessorCreatorSpec.groovy
@@ -1,6 +1,29 @@
+/*
+ * Copyright 2023 Sejin Im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.imsejin.dl.lezhin.process.framework
 
+import spock.lang.Specification
+import spock.lang.Subject
+
+import java.nio.file.Path
+
 import io.github.imsejin.dl.lezhin.process.impl.AccessTokenProcessor
+import io.github.imsejin.dl.lezhin.process.impl.ChromeDriverDownloadProcessor
+import io.github.imsejin.dl.lezhin.process.impl.ChromeInfoResolutionProcessor
 import io.github.imsejin.dl.lezhin.process.impl.ConfigurationFileProcessor
 import io.github.imsejin.dl.lezhin.process.impl.ContentInformationProcessor
 import io.github.imsejin.dl.lezhin.process.impl.DirectoryCreationProcessor
@@ -9,10 +32,6 @@ import io.github.imsejin.dl.lezhin.process.impl.HttpHostsProcessor
 import io.github.imsejin.dl.lezhin.process.impl.LocaleSelectionProcessor
 import io.github.imsejin.dl.lezhin.process.impl.LoginProcessor
 import io.github.imsejin.dl.lezhin.process.impl.PurchasedEpisodesProcessor
-import spock.lang.Specification
-import spock.lang.Subject
-
-import java.nio.file.Path
 
 @Subject(ProcessorCreator)
 class ProcessorCreatorSpec extends Specification {
@@ -31,9 +50,19 @@ class ProcessorCreatorSpec extends Specification {
     def "Creates processors"() {
         given:
         def beans = [Path].collect { Mock(it) }
-        def types = [ConfigurationFileProcessor, LoginProcessor, HttpHostsProcessor, AccessTokenProcessor,
-                     LocaleSelectionProcessor, ContentInformationProcessor, PurchasedEpisodesProcessor,
-                     DirectoryCreationProcessor, DownloadProcessor]
+        def types = [
+                ConfigurationFileProcessor,
+                ChromeInfoResolutionProcessor,
+                ChromeDriverDownloadProcessor,
+                LoginProcessor,
+                HttpHostsProcessor,
+                AccessTokenProcessor,
+                LocaleSelectionProcessor,
+                ContentInformationProcessor,
+                PurchasedEpisodesProcessor,
+                DirectoryCreationProcessor,
+                DownloadProcessor,
+        ]
 
         when:
         def creator = new ProcessorCreator(beans as Object[])
--- src/test/groovy/io/github/imsejin/dl/lezhin/process/framework/ProcessorOrderResolverSpec.groovy
+++ src/test/groovy/io/github/imsejin/dl/lezhin/process/framework/ProcessorOrderResolverSpec.groovy
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2023 Sejin Im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.imsejin.dl.lezhin.process.framework
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+import org.reflections.Reflections
 
 import io.github.imsejin.common.util.ClassUtils
 import io.github.imsejin.dl.lezhin.Application
 import io.github.imsejin.dl.lezhin.process.Processor
 import io.github.imsejin.dl.lezhin.process.impl.AccessTokenProcessor
+import io.github.imsejin.dl.lezhin.process.impl.ChromeDriverDownloadProcessor
+import io.github.imsejin.dl.lezhin.process.impl.ChromeInfoResolutionProcessor
 import io.github.imsejin.dl.lezhin.process.impl.ConfigurationFileProcessor
 import io.github.imsejin.dl.lezhin.process.impl.ContentInformationProcessor
 import io.github.imsejin.dl.lezhin.process.impl.DirectoryCreationProcessor
@@ -12,9 +35,6 @@ import io.github.imsejin.dl.lezhin.process.impl.HttpHostsProcessor
 import io.github.imsejin.dl.lezhin.process.impl.LocaleSelectionProcessor
 import io.github.imsejin.dl.lezhin.process.impl.LoginProcessor
 import io.github.imsejin.dl.lezhin.process.impl.PurchasedEpisodesProcessor
-import org.reflections.Reflections
-import spock.lang.Specification
-import spock.lang.Subject
 
 @Subject(ProcessorOrderResolver)
 class ProcessorOrderResolverSpec extends Specification {
@@ -29,8 +49,11 @@ class ProcessorOrderResolverSpec extends Specification {
 
         then:
         processorTypes == orderedTypes as Set
+
         orderedTypes == [
                 ConfigurationFileProcessor,
+                ChromeInfoResolutionProcessor,
+                ChromeDriverDownloadProcessor,
                 LoginProcessor,
                 HttpHostsProcessor,
                 AccessTokenProcessor,
