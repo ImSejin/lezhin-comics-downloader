b948f6bbbec5fc9cd6274d14b1334fb792af106a
imsejin <hackeres123@gmail.com>
2023-10-18 16:16:30 +0900

    feat: modify chromedriver model `Platform`

--- src/main/java/io/github/imsejin/dl/lezhin/api/chromedriver/model/Platform.java
+++ src/main/java/io/github/imsejin/dl/lezhin/api/chromedriver/model/Platform.java
@@ -19,27 +19,57 @@ package io.github.imsejin.dl.lezhin.api.chromedriver.model;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import io.github.imsejin.common.constant.OS;
+
 import static java.util.stream.Collectors.*;
 
 /**
+ * Platform supported on Chrome product
+ *
  * @since 4.0.0
+ * @see <a href="https://github.com/openjdk/jdk/blob/9843c97695fab3fec1e319027b14974d0e84bf0a/src/hotspot/os/bsd/os_bsd.cpp#L186-L203">
+ *     openjdk/hotspot/os_bsd.cpp</a>
  */
 @Getter
 @ToString
 @RequiredArgsConstructor
 public enum Platform {
 
+    /**
+     * Linux and 64bit processor.
+     */
     LINUX64("linux64"),
+
+    /**
+     * MacOS and Apple Silicon processor.
+     */
     MAC_ARM64("mac-arm64"),
+
+    /**
+     * MacOS and Intel processor.
+     */
     MAC_X64("mac-x64"),
+
+    /**
+     * Windows and 32bit processor.
+     */
     WIN32("win32"),
+
+    /**
+     * Windows and 64bit processor.
+     */
     WIN64("win64");
+
+    private static final String OS_ARCH = System.getProperty("os.arch");
+
+    private static final String PROCESSOR_IDENTIFIER = System.getenv("PROCESSOR_IDENTIFIER");
 
     private static final Map<String, Platform> cache = Arrays.stream(values())
             .collect(collectingAndThen(toMap(it -> it.value, Function.identity()), Collections::unmodifiableMap));
@@ -54,6 +84,31 @@ public enum Platform {
         }
 
         return platform;
+    }
+
+    public static Optional<Platform> getCurrentPlatform() {
+        OS os = OS.getCurrentOS();
+
+        switch (os) {
+            case LINUX:
+            case AIX:
+            case SOLARIS:
+                return Optional.of(LINUX64);
+            case MAC:
+                if (OS_ARCH.contains("aarch") | OS_ARCH.contains("arm")) {
+                    return Optional.of(MAC_ARM64);
+                } else {
+                    return Optional.of(MAC_X64);
+                }
+            case WINDOWS:
+                if (OS_ARCH.contains("64")) {
+                    return Optional.of(WIN64);
+                } else {
+                    return Optional.of(WIN32);
+                }
+        }
+
+        return Optional.empty();
     }
 
 }
